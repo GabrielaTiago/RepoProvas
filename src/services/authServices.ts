75b3d38d0e1d0e8228ed347bf288c32b808024e2
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-15 14:42:51 -0300

    :boom: fix: Fixes the creation of the user

--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -6,13 +6,15 @@ import * as authRepository from '../repositories/authRepository';
 
 export async function login() {}
 
-export async function createUser(user: authInterface.ISignInData) {
-    const userData = await authRepository.findUserEmail(user.email);
+export async function createUser(user: authInterface.ISignUpData) {
+    const email = await authRepository.findUserEmail(user.email);
 
-    if (!userData) {
+    if (email) {
         throw throwCustomError('conflict', 'This e-mail has already been registered');
     }
-
+    if (user.password !== user.confirmPassword){
+        throw throwCustomError('unprocessable_entity', 'The passwords do not match');
+    }
     const encryptedPassword = cryptsPassword(user.password);
     await authRepository.createUser(user.email, encryptedPassword);
 }
