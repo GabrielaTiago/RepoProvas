d16319c4f5f61b305457993b1c53ed40c0a1c2d6
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-15 14:26:48 -0300

    :boom: fix: Add the error handler middleware to the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import 'express-async-errors';
 import express, { json } from 'express';
 import cors from 'cors';
 import router from './routes/routes';
+import { errorHandler } from './middlewares/errorHandlerMiddleware';
 
 const server = express();
 
 server.use(cors(), json());
 server.use(router);
+server.use(errorHandler);
 
 export { server };
