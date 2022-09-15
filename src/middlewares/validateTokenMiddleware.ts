bcd75abb4a6eb01112a0c8300b2c88dd3d39c0db
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-15 15:29:32 -0300

    :sparkles: feat: Middleware to validate the token

---
+++ src/middlewares/validateTokenMiddleware.ts
@@ -0,0 +1,30 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { NextFunction, Request, Response } from 'express';
+import { throwCustomError } from '../errors/throwErrors';
+
+dotenv.config();
+
+export function validatetoken(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        throw throwCustomError('unauthorized', 'Missing authorization header');
+    }
+
+    const token = authorization?.replace('Bearer ', '');
+
+    if (!token) {
+        throw throwCustomError('unauthorized', 'Token not found');
+    }
+
+    try {
+        const JWT_SECRET = process.env.JWT_SECRET;
+        const validToken: any = jwt.verify(token, JWT_SECRET as string);
+
+        res.locals.userId = validToken.id;
+        next();
+    } catch {
+        throw throwCustomError('unauthorized', 'Invalid token');
+    }
+}
