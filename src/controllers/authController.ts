3c5db9a65ef2cb5dfb56e02cb6794c0d8d9848ae
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-15 10:59:23 -0300

    :wrench: chore: Creates the auth interfaces

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
 
+import * as authInterface from '../interfaces/authIntefaces';
+
 export function signUp(req: Request, res: Response) {
-    const user = req.body;
+    const user: authInterface.ISignUpData = req.body;
 
     res.status(201).send('Successfully created user');
 }
 
 export function signIn(req: Request, res: Response) {
-    const user = req.body;
+    const user: authInterface.ISignInData = req.body;
 
     res.status(200).send();
 }
---
+++ src/interfaces/authIntefaces.ts
@@ -0,0 +1,10 @@
+export interface ISignInData {
+    email: string;
+    password: string;
+}
+
+export interface ISignUpData {
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
