51cd98bb064c1b60ef443452391af89704a35f4e
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-19 12:16:34 -0300

    :wrench: chore: Creates the tests teacher controller

--- src/controllers/testsController.ts
+++ src/controllers/testsController.ts
@@ -3,6 +3,11 @@ import { Request, Response } from 'express';
 import * as testsServices from '../services/testsServices';
 
 export async function getTestsByDiscipline(req: Request, res: Response) {
-    const testsByDiscipline = testsServices.showTestsByDiscipline();
+    const testsByDiscipline = await testsServices.showTestsByDiscipline();
     return res.status(200).send(testsByDiscipline);
+}
+
+export async function getTestByTeacher(req: Request, res: Response){
+    const testsByteacher = await testsServices.showTestsByTeacher();
+    return res.status(200).send(testsByteacher);
 }
--- src/routes/testsRoutes.ts
+++ src/routes/testsRoutes.ts
@@ -1,11 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import * as testsController from "../controllers/testsController";
+import * as testsController from '../controllers/testsController';
 
 const testsRouter = Router();
 
-testsRouter.post("/tests")
+testsRouter.post('/tests');
 testsRouter.get('/tests/discipline', testsController.getTestsByDiscipline);
-testsRouter.get('/tests/teacher');
+testsRouter.get('/tests/teacher', testsController.getTestByTeacher);
 
 export default testsRouter;
