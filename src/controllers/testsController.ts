3ec33771c50b66f97844a1125d579d06892ed20a
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-19 15:07:38 -0300

    :sparkles: feat: Creates the insert tests controller

--- src/controllers/testsController.ts
+++ src/controllers/testsController.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from 'express';
+import { ITestData } from '../interfaces/testsInterface';
 
 import * as testsServices from '../services/testsServices';
+
+export async function insertTest(req: Request, res: Response) {
+    const test: ITestData = req.body;
+
+    await testsServices.insertTest(test);
+
+    return res.status(201).send('Test successfully created');
+}
 
 export async function getTestsByDiscipline(req: Request, res: Response) {
     const testsByDiscipline = await testsServices.showTestsByDiscipline();
--- src/routes/testsRoutes.ts
+++ src/routes/testsRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import { validateShemas } from '../middlewares/validateSchemasMiddleware';
 
 import * as testsController from '../controllers/testsController';
 
 const testsRouter = Router();
 
-testsRouter.post('/tests');
+testsRouter.post('/tests', validateShemas('test'), testsController.insertTest);
 testsRouter.get('/tests/discipline', testsController.getTestsByDiscipline);
 testsRouter.get('/tests/teacher', testsController.getTestByTeacher);
 
