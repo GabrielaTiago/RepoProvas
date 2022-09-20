3e634c25959ce802ad1d0175e035a691dc05fd92
GabsTiago <gabrielatiagodearaujo@outlook.com>
2022-09-20 15:27:13 -0300

    :books: docs: Creates the API documentation with SWAGGER

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import "./config/config"
 import 'express-async-errors';
+import swaggerUi from "swagger-ui-express";
 import express, { json } from 'express';
 import cors from 'cors';
 import router from './routes/routes';
 import { errorHandler } from './middlewares/errorHandlerMiddleware';
+import swaggerDocs from "../swagger.json";
 
 const server = express();
 
 server.use(cors(), json());
+
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 server.use(router);
 server.use(errorHandler);
 
