b27b8f0f53d79b2cfb0d8b9e2b8891b53e76b0f2
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-20 13:20:38 -0300

    :recycle: refactor: Simplify validateToken middleware

--- src/middlewares/validateTokenMiddleware.ts
+++ src/middlewares/validateTokenMiddleware.ts
@@ -1,31 +1,17 @@
-import dotenv from 'dotenv';
 import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 
 import { throwCustomError } from '../errors/throwCustomError';
+import { verifyToken } from '../utils/token';
 
-dotenv.config();
-
-export function validatetoken(req: Request, res: Response, next: NextFunction) {
+export function validateToken(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
 
-    if (!authorization) {
-        throw throwCustomError('unauthorized', 'Missing authorization header');
-    }
-
     const token = authorization?.replace('Bearer ', '');
+    if (!token) throw throwCustomError('unauthorized', 'Token JWT not sent');
 
-    if (!token) {
-        throw throwCustomError('unauthorized', 'Token not found');
-    }
+    const validToken = verifyToken(token);
+    if (!validToken) throw throwCustomError('unauthorized', 'Invalid or expired token');
+    res.locals.userId = validToken.id;
 
-    try {
-        const JWT_SECRET = process.env.JWT_SECRET;
-        const validToken: any = jwt.verify(token, JWT_SECRET as string);
-
-        res.locals.userId = validToken.id;
-        next();
-    } catch {
-        throw throwCustomError('unauthorized', 'Invalid token');
-    }
+    next();
 }
