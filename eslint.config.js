c83114a81a3031f6c2bf4ff5a7234984546286da
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-20 12:31:40 -0300

    :wrench: chore: Replace .eslintrc.json with eslint.config.js for improved ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+    {
+        ignores: ['node_modules/', 'dist/', 'jest.config.*'],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.strict,
+    {
+        plugins: {
+            import: importPlugin,
+        },
+        rules: {
+            ...importPlugin.configs.recommended.rules,
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+                    'newlines-between': 'always',
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                },
+            ],
+            'import/no-unresolved': 'off',
+        },
+    },
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+    },
+    prettierConfig
+);
