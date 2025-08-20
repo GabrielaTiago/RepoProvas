0947ba3533a28d3b0d880fb3756900c44f491a57
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-20 14:00:25 -0300

    :recycle: refactor: Update authController to use specific interfaces and improve response messages

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,20 +1,16 @@
 import { Request, Response } from 'express';
 
-import * as authInterface from '../interfaces/authInterfaces';
+import { ISignInData, ISignUpData } from '../interfaces/authInterfaces';
 import * as authServices from '../services/authServices';
 
 export async function signUp(req: Request, res: Response) {
-    const user: authInterface.ISignUpData = req.body;
-
+    const user: ISignUpData = req.body;
     await authServices.createUser(user);
-
-    res.status(201).send('Successfully created user');
+    res.status(201).send({ message: 'User created successfully' });
 }
 
 export async function signIn(req: Request, res: Response) {
-    const user: authInterface.ISignInData = req.body;
-
+    const user: ISignInData = req.body;
     const token = await authServices.login(user);
-
-    res.status(200).send({ token: token, message: 'Authentication Success' });
+    res.status(200).send({ token, message: 'Authentication successful' });
 }
