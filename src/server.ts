b44bb7a98ed2fac9e17797ea175e9728d0a73b42
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-21 19:31:58 -0300

    :bug: fix: Correct typo in server startup log message

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,5 @@ dotenv.config();
 const PORT = Number(process.env.PORT) || 4000;
 
 server.listen(PORT, () => {
-    console.log(`The server is up and runnig on port ${PORT}`);
+    console.log(`The server is up and running on port ${PORT}`);
 });
