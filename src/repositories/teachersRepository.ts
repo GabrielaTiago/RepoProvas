1698aad261b20deff33f9773f708169ff97f4860
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-21 17:41:48 -0300

    :recycle: refactor: Rename function to findTeacherById for clarity

--- src/repositories/teachersRepository.ts
+++ src/repositories/teachersRepository.ts
@@ -1,8 +1,8 @@
 import { database } from '../database/postgres';
 
-export async function checksTheTeacher(teacherId: number) {
+export async function findTeacherById(id: number) {
     const teacher = await database.teacher.findUnique({
-        where: { id: teacherId },
+        where: { id },
     });
     return teacher;
 }
