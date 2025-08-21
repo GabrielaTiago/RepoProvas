8c433733087689221b9e5488a0409421c3d52a81
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-21 17:13:38 -0300

    :recycle: refactor: Rename and update function for clarity and improved error handling

--- src/services/teacherDisciplinesServices.ts
+++ src/services/teacherDisciplinesServices.ts
@@ -1,11 +1,8 @@
 import { throwCustomError } from '../errors/throwCustomError';
 import * as teacherDisciplineRepository from '../repositories/teacherDisciplinesRepository';
 
-export async function checksTheDisciplineAndTheTeacher(teacherId: number, disciplineId: number) {
-    const teacherDiscipline = await teacherDisciplineRepository.checksTheDisciplineAndTheTeacher(teacherId, disciplineId);
-    if (!teacherDiscipline) {
-        throw throwCustomError('not_found', 'This teacher does not teach this subject!');
-    }
-
+export async function findByTeacherAndDisciplineIds(teacherId: number, disciplineId: number) {
+    const teacherDiscipline = await teacherDisciplineRepository.findByTeacherAndDisciplineIds(teacherId, disciplineId);
+    if (!teacherDiscipline) throw throwCustomError('not_found', 'Teacher discipline not found');
     return teacherDiscipline;
 }
