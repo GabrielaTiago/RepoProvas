f1c713dc83de93b276227b30cca4354e8ab1bdfd
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-21 17:42:10 -0300

    :recycle: refactor: Update function to findTeacherById with improved error message

--- src/services/teachersServices.ts
+++ src/services/teachersServices.ts
@@ -1,12 +1,8 @@
 import { throwCustomError } from '../errors/throwCustomError';
 import * as teacherRepository from '../repositories/teachersRepository';
 
-export async function checksTheTeacher(teacherId: number) {
-    const teacher = await teacherRepository.checksTheTeacher(teacherId);
-
-    if (!teacher) {
-        throw throwCustomError('not_found', "This teacher doesn't exits");
-    }
-
+export async function findTeacherById(teacherId: number) {
+    const teacher = await teacherRepository.findTeacherById(teacherId);
+    if (!teacher) throw throwCustomError('not_found', 'Teacher not found');
     return teacher;
 }
