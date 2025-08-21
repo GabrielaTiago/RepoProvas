2d723d76f7a3d9a70af1eaf3a1841ee00684d5d9
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-21 16:49:24 -0300

    :recycle: refactor: Update category service function

--- src/services/categoriesServices.ts
+++ src/services/categoriesServices.ts
@@ -1,12 +1,8 @@
 import { throwCustomError } from '../errors/throwCustomError';
 import * as categoryRepository from '../repositories/categoriesRepository';
 
-export async function checksTheCategory(categoryId: number) {
-    const category = await categoryRepository.checksTheCategory(categoryId);
-
-    if (!category) {
-        throw throwCustomError('not_found', "This category doesn't exits");
-    }
-
+export async function findCategoryById(categoryId: number) {
+    const category = await categoryRepository.findCategoryById(categoryId);
+    if (!category) throw throwCustomError('not_found', 'Category not found');
     return category;
 }
