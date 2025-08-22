76b1cbd034dd72e24e5f8a112a8e56d4a1eeace9
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-08-22 11:22:10 -0300

    :sparkles: feat: Add mock test creation function for testing

--- tests/factory/testsFactory.ts
+++ tests/factory/testsFactory.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+import { Test } from '@prisma/client';
 
 export async function __createValidTests() {
     const test = {
@@ -9,4 +10,18 @@ export async function __createValidTests() {
         teacherId: 2,
     };
     return test;
+}
+
+export function __createMockTest(overrides?: Partial<Test>) {
+    return {
+        id: faker.number.int({ min: 1, max: 100 }),
+        name: faker.lorem.words(3),
+        pdfUrl: faker.internet.url(),
+        categoryId: faker.number.int({ min: 1, max: 100 }),
+        disciplineId: faker.number.int({ min: 1, max: 100 }),
+        teacherId: faker.number.int({ min: 1, max: 100 }),
+        teacherDisciplineId: faker.number.int({ min: 1, max: 100 }),
+        createdAt: faker.date.recent(),
+        ...overrides,
+    };
 }
